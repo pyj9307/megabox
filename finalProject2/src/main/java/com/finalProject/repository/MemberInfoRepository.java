0febbadc9d1f0e0369d9e6e0bf3834124195f4c6
ADMIN <ADMIN@DESKTOP-RCVKJK6.mshome.net>
2023-02-07 15:52:46 +0900

    shop으로 가져와서 create까지는 완성인데 roadFullAddr이 road_Full_Addr로 오라클디비 컬럼이 들어가는 오류 발생. 직접 sql로 컬럼 수정해서 만들어주는 중(roadFullAddr 버릴까...)

--- finalProject2/src/main/java/com/finalProject/repository/MemberInfoRepository.java
+++ finalProject2/src/main/java/com/finalProject/repository/MemberInfoRepository.java
@@ -1,10 +1,10 @@
-//package com.finalProject.repository;
-//
-//import com.finalProject.entity.MemberInfo;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MemberInfoRepository extends JpaRepository<MemberInfo, Long> {
-//	// 일단 shop에서 따온대로 보류.
-//    MemberInfo findByEmail(String email);
-//
-//}
+package com.finalProject.repository;
+
+import com.finalProject.entity.MemberInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberInfoRepository extends JpaRepository<MemberInfo, Long> {
+	// 일단 shop에서 따온대로 보류.
+    MemberInfo findByEmail(String email);
+
+}
